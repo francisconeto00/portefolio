154bf2bd0f9f2ebd17f2554bf43bf691df60cc7a
Francisco Neto <francisconeto@MBPdeFrancisco.lan>
2023-07-31 18:43:40 +0100

    colors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,87 @@
-import { Box, Container, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import {
+  Box,
+  Container,
+  Typography,
+  createTheme,
+  ThemeProvider,
+} from "@mui/material";
 import "./App.scss";
+import { light } from "@mui/material/styles/createPalette";
+
+const theme = createTheme({
+  palette: { // Correção do nome do atributo "palette"
+    primary: {
+      main: "#25283D",
+    },
+    secondary: {
+      main: "#8F3985",
+    },
+    info: {
+      main: "#98DFEA",
+    },
+  },
+});
 
 function App() {
+  const [scrolling, setScrolling] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setScrolling(true);
+      } else {
+        setScrolling(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="container">
-      <Typography variant="h1"></Typography>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className={`container ${scrolling ? "scrolling" : ""}`}>
+        <Typography color={"info.main"} fontSize={scrolling ? "17px": "20px"}>
+          Seu conteúdo aqui
+        </Typography>
+      </div>
+      <div className="title-container">
+
+      <Typography color={"info.main"} variant="h1">
+          Seu conteúdo aqui
+        </Typography>
+        <Typography color={"info.main"} variant="h1">
+          Seu conteúdo aqui
+        </Typography>
+        <Typography color={"info.main"} variant="h1">
+          Seu conteúdo aqui
+        </Typography>
+        <Typography color={"info.main"} variant="h1">
+          Seu conteúdo aqui
+        </Typography>
+        <Typography color={"info.main"} variant="h1">
+          Seu conteúdo aqui
+        </Typography>
+        <Typography color={"info.main"} variant="h1">
+          Seu conteúdo aqui
+        </Typography>
+        <Typography color={"info.main"} variant="h1">
+          Seu conteúdo aqui
+        </Typography>
+        <Typography color={"info.main"} variant="h1">
+          Seu conteúdo aqui
+        </Typography>
+        <Typography color={"info.main"} variant="h1">
+          Seu conteúdo aqui
+        </Typography>
+        </div>
+       
+
+    </ThemeProvider>
   );
 }
 
